63988a733593021796d89ab00eaa34364789fe56
josep51856 <132079680+josep51856@users.noreply.github.com>
2025-08-07 09:40:23 -0300

    salvo

--- main.js
+++ main.js
@@ -8,11 +8,9 @@ const simbolos = '!@%*?';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
-const forcaSenha = document.querySelector ('.forca')
-
+const forcaSenha = document.querySelector ('.forca');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
-
 function diminuiTamanho() {
     if (tamanhoSenha > 1) {
         tamanhoSenha--;
@@ -27,15 +25,11 @@ function aumentaTamanho() {
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
-
 for (i = 0; i < checkbox.length; i++) {
     checkbox[i].onclick = geraSenha;
 }
-
 geraSenha();
-
 function geraSenha() {
-
     let alfabeto = '';
     if (checkbox[0].checked) {
         alfabeto = alfabeto + letrasMaiusculas;
@@ -59,11 +53,10 @@ function geraSenha() {
     campoSenha.value = senha;
     classificaSenha (alfabeto.length);
 }
-
 function classificaSenha(tamanhoAlfabeto){
-    let entropia = tamanhoSenha * Math.log2 (tamanhoAlfabeto);
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
     forcaSenha.classList.remove('fraca','media','forte');
-        if (entropia > 56){
+        if (entropia > 57){
            forcaSenha.classList.add('forte');
         } else if (entropia > 35 && entropia <= 57) {
             forcaSenha.classList.add('media');
@@ -71,5 +64,6 @@ function classificaSenha(tamanhoAlfabeto){
             forcaSenha.classList.add('fraca');
         }
         const valorEntropia = document.querySelector('.entropia');
-        valorEntropia.textContent = "Um computador pode levar até" + Math.floor(2**entropia/(100e6*60*60*24)) + "dias para descobrir essa senha.";
+        valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+        valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }
